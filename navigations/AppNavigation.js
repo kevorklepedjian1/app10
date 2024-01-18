37511b603ffac8a52eddb62e8ea68df39f5fdeed
User <kedvorkbedroslepedjian@gmail.com>
2024-01-18 16:58:48 +0200

    first commit

---
+++ navigations/AppNavigation.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import { LogBox, Text, View } from 'react-native';
+
+import DetailsScreen from '../screens/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+LogBox.ignoreLogs([
+  'Non-serializable values were found in the navigation state',
+]);
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="HomeScreen" component={HomeScreen}  />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+  
+}
